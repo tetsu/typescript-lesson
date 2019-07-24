5663ea8b04169fa669b9b9e1da1ece72b8e6a649
tetsuromori <tetsuromori@gmail.com>
2019-07-24 14:39:05 +0900

    finish section 7

---
+++ features/annotations/interfaces.ts
@@ -0,0 +1,42 @@
+interface Vehicle {
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string;
+}
+
+interface Reportable {
+  summary(): string;
+}
+
+const oldCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+};
+
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar.`;
+  }
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
+  console.log(vehicle.summary());
+};
+
+const printSummary = (report: Reportable): void => {
+  console.log(report.summary());
+};
+
+printVehicle(oldCivic);
+
+printSummary(oldCivic);
+
+printSummary(drink);
