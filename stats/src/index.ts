d24c2bde424490f231d67e0468a061a677689285
tetsuromori <tetsuromori@gmail.com>
2019-07-26 16:52:18 +0900

    finish lesson 104

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,46 @@
-console.log('Hi');
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map(
+    (row: string): string[] => {
+      return row.split(',');
+    }
+  );
+
+let manUnitedWins = 0;
+
+enum MatchRecord {
+  Date = 0,
+  HomeTeam = 1,
+  AwayTeam = 2,
+  HomeScore = 3,
+  AwayScore = 4,
+  Winner = 5,
+  RefereeName = 6
+}
+
+enum Winner {
+  HomeTeam = 'H',
+  AwayTeam = 'A',
+  Draw = 'D'
+}
+
+for (let match of matches) {
+  if (
+    match[MatchRecord.HomeTeam] === 'Man United' &&
+    match[MatchRecord.Winner] === Winner.HomeTeam
+  ) {
+    manUnitedWins++;
+  } else if (
+    match[MatchRecord.AwayTeam] === 'Man United' &&
+    match[MatchRecord.Winner] === Winner.AwayTeam
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`);
