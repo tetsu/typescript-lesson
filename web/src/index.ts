8af6960ec1c008caded7cc10a80dfa74d18ff041
tetsuromori <tetsuromori@gmail.com>
2019-07-30 15:28:37 +0900

    finish lesson 167

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,9 +2,4 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Jason', age: 20 });
 
-user.save();
-
-console.log(user);
-
-const user2 = new User({ id: 1, name: 'James', age: 20 });
-user2.save();
+user.attributes.get('id');
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,46 +1,22 @@
-import axios, { AxiosResponse } from 'axios';
+// import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
+import { Attributes } from './Attributes';
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
 
+const url = 'http://localhost:3000/users';
+
 export class User {
-  events: Eventing = new Eventing();
-  constructor(private data: UserProps) {}
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(url);
+  public attributes: Attributes<UserProps>; // = new Attributes<UserProps>({})
 
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`).then(
-      (response: AxiosResponse): void => {
-        this.set(response.data);
-      }
-    );
-  }
-
-  save(): void {
-    const id = this.get('id');
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data).then(
-        (response: AxiosResponse): void => {
-          this.set(response.data);
-        }
-      );
-    } else {
-      axios.post(`http://localhost:3000/users/`, this.data).then(
-        (response: AxiosResponse): void => {
-          this.set(response.data);
-        }
-      );
-    }
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
